22398e150c110ed90c720e9a34387bd56a14489c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-18 15:51:02 -0700

    listed view now renders on route /home

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 
 urlpatterns = [
-    path('', main)
+    path('home', RoomView.as_view()),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,7 +4,7 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     
