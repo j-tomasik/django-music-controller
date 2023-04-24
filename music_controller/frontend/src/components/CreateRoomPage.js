0f565b9e2c5ba7dfc8b7d0be8c7b5dc64177200c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-24 15:22:22 -0700

    starting to build create room components

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import{ Button, Grid, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from'@mui/material'
+import {Link } from 'react-router-dom'
+
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p>This is the create room page</p>
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component='h4' variant='h4'></Typography>
+                </Grid>
+            </Grid>
+        )
     }
 
 
