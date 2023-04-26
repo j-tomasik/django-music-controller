47fd19677bf013a6c0b77723d99bfd34c20ef25c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-26 15:20:51 -0700

    now working

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,25 +1,20 @@
-import React , { Component } from 'react';
-import {createRoot} from 'react-dom/client';
-import Homepage from './HomePage';
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                hello
-                <Homepage />
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-const appDiv = document.getElementById('app');
-const root = createRoot(appDiv)
-root.render(<App tab='home' />)
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-export default class Homepage extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-        <Router>
-            <div>
-                <div>Please help me</div>
-                <Routes>
-                    <Route  exact path="/">
-                    <p>This is the Home Page</p>
-                    </Route>
-                    <Route path='/join' element={<RoomJoinPage />} />
-                    <Route path='/create' component={CreateRoomPage} />
-                </Routes>
-            </div>
-        </Router>
-        )
-    // }
-    }
-
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home PAGE</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
