c8a94147b7c9c91190b7bdb2ff0068264d0b1a89
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-28 14:11:03 -0700

    built out html for home page

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,12 +16,34 @@ export default class HomePage extends Component {
     super(props);
   }
 
+  renderHomPage() {
+    return(
+      <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant='h3' compact='h3'>
+              House Party
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant='contained' color='primary'>
+              <Button color='primary' to='/join' component={Link}>
+                Join a Room
+              </Button>
+              <Button color='secondary' to='/create' component={Link}>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+      </Grid>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <p>This is the home PAGE</p>
+            {this.renderHomPage()}
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
