44de7728d120b0a2a0255c68ae112ef5baa871b2
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-05-02 18:01:34 -0700

    Setting up hidden keys in env file

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)9w$f!w^zwvf4@=a(x%l#%vi3yiuvr0^tpxem1$zg^by&6fu2'
+# 'django-insecure-%)9w$f!w^zwvf4@=a(x%l#%vi3yiuvr0^tpxem1$zg^by&6fu2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ music_controller/spotfiy/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from music_controller.settings import CLIENT_ID, CLIENT_SECRET, REDIREC_URI
+from rest_framework.views import APIView
+
+class AuthURL(APIView):
+
+
+
+
