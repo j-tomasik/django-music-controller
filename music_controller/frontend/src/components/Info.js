73e94d07db6f1e56cf9e5def830a516c96cc416f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-05-10 16:02:10 -0700

    added better instructions to info page

--- music_controller/frontend/src/components/Info.js
+++ music_controller/frontend/src/components/Info.js
@@ -13,11 +13,33 @@ export default function Info(props){
     const [page, setPage] = useState(pages.JOIN);
 
     function joinInfo(){
-        return "JOIN PAGE"
+        return <Grid container spacing={4}>
+            <Typography variant='h3'>Join a Room Info</Typography>
+            <Typography variant='body1'>
+                Once someone else has created a room and generated a key and shared it with you...</Typography>
+            <Typography variant='body2'>
+                Hit join a room, enter the room key code then enjoy their music and exercise your ability to play, pause and skip the song!
+            </Typography>
+        </Grid>
     }
 
     function createInfo(){
-        return "CREATE PAGE"
+        return <Grid container spacing={1}>
+            <Typography variant='h3'>Create a Room Info</Typography>
+            <Typography variant='body1'>
+                Allow for shared control of a what song your Spotify is playing.
+            </Typography>
+            <Typography variant='body2'>
+                1. Play a song or playlist on your Spotify.
+                2. Create a Room and pick your settings.
+                    -Set up how many votes by room members are needed to skip a song.
+                    -Decide if room members are allowed to pause and play the current song.
+                3. Your room code can now be shared with others who can join and have the music play for them remotely.
+                4. As the host you can edit the room settings to enable or disable previous settings and change the votes needed to skip.
+                5. If you created the room you are the host. If the host leaves the room, the room will be deleted automatically.
+                6. Enjoy!
+            </Typography>
+        </Grid>
     }
 
     return(
