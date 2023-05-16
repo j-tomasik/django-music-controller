99d784dfed5305660da0158af74c1c4ae4175ae2
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-05-16 09:39:11 -0700

    finished with info page styling

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -10,39 +10,39 @@ const pages = {
 }
 
 export default function Info(props){
-    const [page, setPage] = useState(pages.JOIN);
+    const [page, setPage] = useState(pages.CREATE);
 
     function joinInfo(){
-        return <Grid container>
+        return <Grid container spacing={4}>
             <Grid item align='center' xs={12}>
-                <Typography component='h2' variant='h4' mt={12}>Join a Room Info!</Typography>
-                <Typography variant='body1'>
-                Once someone else has created a room and generated a key and shared it with you...</Typography>
+                <Typography style={{ margin: '8px' }} component='h2' variant='h4' mt={12}>Join a Room Info!</Typography>
+                <Typography style={{ margin: '8px' }} variant='body1'>
+                Once someone else has created a room and shared they key code they generated with you...</Typography>
             
-            <Typography variant='body2'>
-                Hit join a room, enter the room key code then enjoy their music and exercise your ability to play, pause and skip the song!
+            <Typography style={{ margin: '8px' }} variant='body1'>
+                Hit join a room from our home page, enter the room key code then enjoy their music and exercise your ability to play, pause and skip the song!
             </Typography>
             </Grid>
         </Grid>
     }
 
     function createInfo(){
-        return <Grid container>
+        return <Grid container spacing={8}>
             <Grid item align='center' xs={12}>
-                <Typography component='h2' variant='h4'>Create a Room Info!</Typography>
-                <Typography variant='body1'>
+                
+                <Typography style={{ margin: '8px' }} variant='body1' >
                     Allow for shared control of a what song your Spotify is playing.
-                </Typography>
-            
+                </Typography >
+                <Typography style={{ margin: '8px' }} component='h2' variant='h4'>Create a Room Info!</Typography>
             <Grid item>
-                <Typography>1. Play a song or playlist on your Spotify.</Typography>
-                <Typography>2. Create a Room and pick your settings.</Typography>
-                <Typography>3. Set up how many votes by room members are needed to skip a song.</Typography>
-                <Typography>4. Decide if room members are allowed to pause and play the current song.</Typography>
-                <Typography>5. Your room code can now be shared with others who can join and have the music play for them remotely.</Typography>
-                <Typography>6. As the host you can edit the room settings to enable or disable previous settings and change the votes needed to skip.</Typography>
-                <Typography>7. If you created the room you are the host. If the host leaves the room, the room will be deleted automatically.</Typography>
-                <Typography>8. Enjoy!</Typography>
+                <Typography style={{ margin: '8px' }} >1. Play a song or playlist on your Spotify.</Typography>
+                <Typography style={{ margin: '8px' }} >2. Create a Room and pick your room's settings.</Typography>
+                <Typography style={{ margin: '8px' }} >3. Set up how many votes by room members are needed to skip a song.</Typography>
+                <Typography style={{ margin: '8px' }} >4. Decide if room members are allowed to pause and play the current song.</Typography>
+                <Typography style={{ margin: '8px' }} >5. Your room code can now be shared with others who can join and have the music play for them remotely.</Typography>
+                <Typography style={{ margin: '8px' }} >6. As the host you can edit the room settings to enable or disable previous settings and change the votes needed to skip.</Typography>
+                <Typography style={{ margin: '8px' }} >7. If you created the room you are the host. If the host leaves the room, the room will be deleted automatically.</Typography>
+                <Typography style={{ margin: '8px' }} >8. Enjoy!</Typography>
             </Grid>
             </Grid>
         </Grid>
@@ -63,7 +63,7 @@ export default function Info(props){
                     <IconButton onClick={() => {
                         page === pages.CREATE ? setPage(pages.JOIN) : setPage(pages.CREATE);
                     }}>
-                        {page === pages.CREATE ? <NavigateBeforeIcon/> : <NavigateNextIcon/>}
+                        {page === pages.CREATE ? <NavigateNextIcon/> : <NavigateBeforeIcon/>}
                     </IconButton>
                 </Grid>
                 <Grid item xs={12} align='center'>
